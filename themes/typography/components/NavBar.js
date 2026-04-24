f0f18101ce2e5ebf3904cf027c7e8834c5b904e9
azuoand-png <azuoand@gmail.com>
2026-04-24 17:35:45 +0800

    Update NavBar.js

--- themes/typography/components/NavBar.js
+++ themes/typography/components/NavBar.js
@@ -9,16 +9,16 @@ import SmartLink from '@/components/SmartLink'
 export default function NavBar(props) {
   return (
     <div className='flex flex-col justify-between md:mt-8 md:h-auto'>
-      {/* 为 header 添加一个自定义类，便于 CSS 控制 hover 背景 */}
-      <header className='relative w-fit self-center md:self-start md:pb-8 md:border-l-2 dark:md:border-white dark:text-white md:border-[var(--primary-color)] text-[var(--primary-color)] md:[writing-mode:vertical-lr] px-4 mb-2 md:ml-8 nav-header-hover'>
+      {/* 只需要添加 z-10 确保 hover 区域覆盖，其他类保持不变 */}
+      <header className='relative w-fit self-center md:self-start md:pb-8 md:border-l-2 dark:md:border-white dark:text-white md:border-[var(--primary-color)] text-[var(--primary-color)] md:[writing-mode:vertical-lr] px-4 hover:bg-[var(--primary-color)] dark:hover:bg-white hover:text-white dark:hover:text-[var(--primary-color)] ease-in-out duration-700 md:hover:pt-4 md:hover:pb-4 mb-2 md:ml-8 z-10'>
         <SmartLink href='/'>
           <div className='flex flex-col-reverse md:flex-col items-center md:items-start'>
             <div className='font-bold text-4xl text-center' id='blog-name'>
               {siteConfig('TYPOGRAPHY_BLOG_NAME')}
             </div>
-            {/* 绝对定位，保持英文名在竖线左侧 */}
+            {/* 给绝对定位的英文名添加 pointer-events-none，避免干扰 */}
             <div
-              className='font-bold text-xl text-center absolute'
+              className='font-bold text-xl text-center absolute pointer-events-none'
               style={{
                 writingMode: 'vertical-lr',
                 left: '-2rem',
