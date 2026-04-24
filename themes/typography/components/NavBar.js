ca1bf592ee44aa1145c525092bf88560b57af098
azuoand-png <azuoand@gmail.com>
2026-04-24 17:05:32 +0800

    Update NavBar.js

--- themes/typography/components/NavBar.js
+++ themes/typography/components/NavBar.js
@@ -13,20 +13,24 @@ import SmartLink from '@/components/SmartLink'
  */
 export default function NavBar(props) {
   return (
-    <div className='flex flex-col justify-between md:mt-20 md:h-[70vh]'>
-      <header className='w-fit self-center md:self-start md:pb-8 md:border-l-2 dark:md:border-white dark:text-white md:border-[var(--primary-color)] text-[var(--primary-color)] md:[writing-mode:vertical-lr] px-4 hover:bg-[var(--primary-color)] dark:hover:bg-white hover:text-white dark:hover:text-[var(--primary-color)] ease-in-out duration-700 md:hover:pt-4 md:hover:pb-4 mb-2'>
+    // 外层容器：高度自适应，顶部外边距缩小，使菜单上移
+    <div className='flex flex-col justify-between md:mt-8 md:h-auto'>
+      {/* 标题区域：整体向右移动 1rem，英文名向左移动 1rem 越过竖线 */}
+      <header className='w-fit self-center md:self-start md:pb-8 md:border-l-2 dark:md:border-white dark:text-white md:border-[var(--primary-color)] text-[var(--primary-color)] md:[writing-mode:vertical-lr] px-4 hover:bg-[var(--primary-color)] dark:hover:bg-white hover:text-white dark:hover:text-[var(--primary-color)] ease-in-out duration-700 md:hover:pt-4 md:hover:pb-4 mb-2 md:ml-4'>
         <SmartLink href='/'>
           <div className='flex flex-col-reverse md:flex-col items-center md:items-start'>
             <div className='font-bold text-4xl text-center' id='blog-name'>
               {siteConfig('TYPOGRAPHY_BLOG_NAME')}
             </div>
-            <div className='font-bold text-xl text-center' id='blog-name-en'>
+            {/* 英文名向左移动 1rem，显示在竖线左侧 */}
+            <div className='font-bold text-xl text-center relative md:-left-4' id='blog-name-en'>
               {siteConfig('TYPOGRAPHY_BLOG_NAME_EN')}
             </div>
           </div>
         </SmartLink>
       </header>
-      <nav className='md:pt-0  z-20   flex-shrink-0'>
+      {/* 为菜单容器添加一个自定义类，方便全局 CSS 控制样式 */}
+      <nav className='md:pt-0 z-20 flex-shrink-0 menu-custom'>
         <div id='nav-bar-inner' className='text-sm md:text-md'>
           <MenuList {...props} />
         </div>
