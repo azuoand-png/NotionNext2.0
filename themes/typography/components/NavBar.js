30603134450e55bfb8fc4aa2563fc32f500059af
azuoand-png <azuoand@gmail.com>
2026-04-24 23:27:17 +0800

    Update NavBar.js

--- themes/typography/components/NavBar.js
+++ themes/typography/components/NavBar.js
@@ -13,8 +13,8 @@ import SmartLink from '@/components/SmartLink'
  */
 export default function NavBar(props) {
   return (
-    /* 重点修改：在下方 className 末尾增加了 md:pr-12，数值越大，离右边缘越远 */
-    <div className='flex flex-col justify-between md:mt-20 md:h-[70vh] md:pr-12'>
+    /* 修改点：添加了 md:-mr-12 (负右边距)，数值越大，这一列就越往右边贴近屏幕边缘 */
+    <div className='flex flex-col justify-between md:mt-20 md:h-[70vh] md:-mr-12'>
       <header className='w-fit self-center md:self-start md:pb-8 md:border-l-2 dark:md:border-white dark:text-white md:border-[var(--primary-color)] text-[var(--primary-color)] md:[writing-mode:vertical-lr] px-4 hover:bg-[var(--primary-color)] dark:hover:bg-white hover:text-white dark:hover:text-[var(--primary-color)] ease-in-out duration-700 md:hover:pt-4 md:hover:pb-4 mb-2'>
         <SmartLink href='/'>
           <div className='flex flex-col-reverse md:flex-col items-center md:items-start'>
