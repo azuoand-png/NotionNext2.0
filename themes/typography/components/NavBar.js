1a1656d5520a86d12d07d479bd7a0b3d1323a559
azuoand-png <azuoand@gmail.com>
2026-04-24 23:50:38 +0800

    Update NavBar.js

--- themes/typography/components/NavBar.js
+++ themes/typography/components/NavBar.js
@@ -13,11 +13,16 @@ import SmartLink from '@/components/SmartLink'
  */
 export default function NavBar(props) {
   return (
-    /* 1. md:-mr-48: 我们给它一个足够大的负值，确保它能“飞”出原本受限的区域。
-      2. md:pl-60: 这是 15rem 的左侧留白，确保文字左侧离文章区侧线永远隔着 15rem。
-      3. md:pr-4: 这是右侧留白，数值越小，文字越贴近屏幕最右边缘。
-    */
-    <div className='flex flex-col justify-between md:mt-20 md:h-[70vh] md:-mr-48 md:pl-60 md:pr-4'>
+    <div 
+      className='flex flex-col justify-between md:mt-20 md:h-[70vh]'
+      /* marginRight: 控制整体向右平移，数值越负（如-160px）越贴边
+         paddingLeft: 15rem 确保与文章区的间隔
+      */
+      style={{
+        marginRight: '-140px', 
+        paddingLeft: '15rem'   
+      }}
+    >
       <header className='w-fit self-center md:self-start md:pb-8 md:border-l-2 dark:md:border-white dark:text-white md:border-[var(--primary-color)] text-[var(--primary-color)] md:[writing-mode:vertical-lr] px-4 hover:bg-[var(--primary-color)] dark:hover:bg-white hover:text-white dark:hover:text-[var(--primary-color)] ease-in-out duration-700 md:hover:pt-4 md:hover:pb-4 mb-2'>
         <SmartLink href='/'>
           <div className='flex flex-col-reverse md:flex-col items-center md:items-start'>
@@ -30,7 +35,7 @@ export default function NavBar(props) {
           </div>
         </SmartLink>
       </header>
-      <nav className='md:pt-0  z-20   flex-shrink-0'>
+      <nav className='md:pt-0 z-20 flex-shrink-0'>
         <div id='nav-bar-inner' className='text-sm md:text-md'>
           <MenuList {...props} />
         </div>
