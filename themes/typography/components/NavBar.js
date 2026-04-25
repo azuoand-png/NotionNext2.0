4da987017d7960aba5f01a68a91dad24f39dea9d
azuoand-png <azuoand@gmail.com>
2026-04-25 20:51:45 +0800

    Update NavBar.js

--- themes/typography/components/NavBar.js
+++ themes/typography/components/NavBar.js
@@ -5,8 +5,9 @@ import SmartLink from '@/components/SmartLink'
 
 export default function NavBar(props) {
   return (
-    <div className='flex flex-col'>
-      <header className='w-fit self-start md:pb-8 md:border-l-2 dark:md:border-white dark:text-white md:border-[var(--primary-color)] text-[var(--primary-color)] md:[writing-mode:vertical-lr] px-4 hover:bg-[var(--primary-color)] dark:hover:bg-white hover:text-white dark:hover:text-[var(--primary-color)] ease-in-out duration-700 md:hover:pt-4 md:hover:pb-4 mb-2'>
+    // 移除内部可能的多余 padding，整体紧贴右侧
+    <div className='flex flex-col items-end'>
+      <header className='w-fit self-end md:pb-8 md:border-l-2 dark:md:border-white dark:text-white md:border-[var(--primary-color)] text-[var(--primary-color)] md:[writing-mode:vertical-lr] px-4 hover:bg-[var(--primary-color)] dark:hover:bg-white hover:text-white dark:hover:text-[var(--primary-color)] ease-in-out duration-700 md:hover:pt-4 md:hover:pb-4 mb-2'>
         <SmartLink href='/'>
           <div className='flex flex-col items-start'>
             <div className='font-bold text-4xl text-center' id='blog-name'>
@@ -18,11 +19,11 @@ export default function NavBar(props) {
           </div>
         </SmartLink>
       </header>
-      <nav className='md:pt-4 z-20 flex-shrink-0'>
-        <div id='nav-bar-inner' className='text-sm md:text-md'>
+      <nav className='md:pt-4 z-20 flex-shrink-0 w-full'>
+        <div id='nav-bar-inner' className='text-sm md:text-md text-right'>
           <MenuList {...props} />
         </div>
-        <div className='mt-4'>
+        <div className='mt-4 flex justify-end'>
           <SocialButton />
         </div>
       </nav>
