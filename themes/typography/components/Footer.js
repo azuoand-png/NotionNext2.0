fa595fcac5d18a1fbba3790038d2be821480a7c1
azuoand-png <azuoand@gmail.com>
2026-04-25 13:50:38 +0800

    Update Footer.js

--- themes/typography/components/Footer.js
+++ themes/typography/components/Footer.js
@@ -20,8 +20,8 @@ export default function Footer(props) {
         /* 强制将黑暗模式按钮固定在右上角 */
         .fixed-dark-button {
           position: fixed !important;
-          top: 1.5rem;   /* 距离顶部距离 */
-          right: 1.5rem; /* 距离右侧距离 */
+          top: 3rem;   /* 距离顶部距离 */
+          right: 3rem; /* 距离右侧距离 */
           z-index: 100;  /* 确保在最上层 */
           cursor: pointer;
           background: transparent;
@@ -29,8 +29,8 @@ export default function Footer(props) {
 
         /* 如果按钮内图标太小，可以微调 */
         .fixed-dark-button svg {
-           width: 1.25rem;
-           height: 1.25rem;
+           width: 1.7rem;
+           height: 1.7rem;
         }
       `}</style>
 
