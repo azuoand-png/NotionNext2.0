0c5f752c62301301fef14eed210538309f2e8c47
azuoand-png <azuoand@gmail.com>
2026-04-26 15:37:58 +0800

    Update JumpToTopButton.js

--- themes/typography/components/JumpToTopButton.js
+++ themes/typography/components/JumpToTopButton.js
@@ -1,14 +1,6 @@
 import { useGlobal } from '@/lib/global'
 import { useEffect, useState } from 'react'
 
-/**
- * 跳转到网页顶部
- * 当屏幕下滑500像素后会出现该控件
- * @param targetRef 关联高度的目标html标签
- * @param showPercent 是否显示百分比
- * @returns {JSX.Element}
- * @constructor
- */
 const JumpToTopButton = () => {
   const { locale } = useGlobal()
   const [show, switchShow] = useState(false)
@@ -25,11 +17,15 @@ const JumpToTopButton = () => {
     return () => document.removeEventListener('scroll', scrollListener)
   }, [show])
 
-  return <div title={locale.POST.TOP}
-        className={(show ? ' opacity-100 ' : 'invisible  opacity-0') + ' transition-all duration-300 flex items-center justify-center cursor-pointer bg-black h-10 w-10 bg-opacity-40 rounded-sm'}
-        onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-    ><i className='fas fa-angle-up text-white ' />
+  return (
+    <div
+      title={locale.POST.TOP}
+      className={(show ? 'opacity-100' : 'invisible opacity-0') + ' transition-all duration-300 flex items-center justify-center cursor-pointer bg-black h-10 w-10 bg-opacity-40 rounded-sm z-50'}
+      onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+    >
+      <i className='fas fa-angle-up text-white' />
     </div>
+  )
 }
 
 export default JumpToTopButton
