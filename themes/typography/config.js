6722dad0aff6af28a36a1ec3ac63a1ae887ce4e7
azuoand-png <azuoand@gmail.com>
2026-04-27 15:48:14 +0800

    Update config.js

--- themes/typography/config.js
+++ themes/typography/config.js
@@ -14,7 +14,16 @@ const CONFIG = {
   TYPOGRAPHY_MENU_TAG: true, // 显示标签
   TYPOGRAPHY_MENU_ARCHIVE: true, // 显示归档
 
-  // 不蒜子统计（新增）
+  // 不蒜子统计
   ANALYTICS_BUSUANZI_SITE_ID: true,
+
+  // ========== 新增：Heo 风格个人资料底部按钮配置 ==========
+  HEO_INFO_CARD_URL1: 'https://kexuelanguage.vercel.app/contact',                     // 第一个按钮的链接
+  HEO_INFO_CARD_ICON1: 'fas fa-user',               // 第一个按钮的图标
+  HEO_INFO_CARD_URL2: 'https://kexuelanguage.vercel.app/channel',
+  HEO_INFO_CARD_ICON2: 'fab fa-github',
+  HEO_INFO_CARD_URL3: 'https://kexuelanguage.vercel.app/support',
+  HEO_INFO_CARD_TEXT3: '支持打赏',                   // 第三个按钮的文字（带箭头）
 }
+
 export default CONFIG
